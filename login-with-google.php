443b66b82e641b32575d5d60e5a054a4186abb12
Ikbal <ikbalsidhu7@gmail.com>
2026-05-15 18:04:21 +0530

    v1.0.1 - Allow Google sign-ups when WP registration is disabled

--- login-with-google.php
+++ login-with-google.php
@@ -3,7 +3,7 @@
  * Plugin Name: Login with Google
  * Plugin URI: https://github.com/appcoderikbal/Login-with-google-wp-plugin
  * Description: Allow users to login or register on your WordPress site using their Google account. Provides a shortcode [login_with_google] to display the button.
- * Version: 1.0.0
+ * Version: 1.0.1
  * Author: Ikbal Singh
  * Author URI: https://github.com/appcoderikbal
  * License: GPL v2 or later
@@ -18,7 +18,7 @@ if (!defined('ABSPATH')) {
     exit;
 }
 
-define('LWG_VERSION', '1.0.0');
+define('LWG_VERSION', '1.0.1');
 define('LWG_PLUGIN_DIR', plugin_dir_path(__FILE__));
 define('LWG_PLUGIN_URL', plugin_dir_url(__FILE__));
 define('LWG_PLUGIN_FILE', __FILE__);
